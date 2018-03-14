651e12dcb499ead9329551489f885b85ced3b3aa
hudd <hudd@bairuitech.com>
2018-03-14 18:50:41 +0800

    创建项目结构
    Change-Id: I7186c57dbb6866e20306e8b5be0b44932f36a611

---
+++ src/views/Hello/hello.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="hello">
+  <ul>
+    <li>我是hello</li>
+    <li v-for="(item,key) in data">{{item}}</li>
+  </ul>
+  <span></span>
+</div>
+</template>
+
+<script>
+export default {
+  name:'hello',
+  data(){
+    return {
+      data:[]
+    }
+  },
+  mounted:function(){
+    this.Axios.get('/api/data').then(res=>{
+      this.data=res.data;
+      console.info(res)
+    })
+  }
+}
+</script>
+
